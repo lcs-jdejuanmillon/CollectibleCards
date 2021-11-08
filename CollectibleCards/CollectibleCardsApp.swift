50b96db2d2383715b3f1d1294a3f74ebf0331de4
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-11-08 11:16:43 -0500

    Initial Commit

---
+++ CollectibleCards/CollectibleCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectibleCardsApp.swift
+//  CollectibleCards
+//
+//  Created by Jacobo de Juan Millon on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct CollectibleCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
