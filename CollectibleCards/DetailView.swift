6b7da0261c012c3630c200e3db5a2be996bba5c8
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-11-08 19:50:49 -0500

    Changes to Messi view

--- CollectibleCards/DetailView.swift
+++ CollectibleCards/DetailView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Jacobo de Juan Millon on 2021-11-08.
 //
-
+//Messi, Ronaldo, Neymar, Mbappé, Ramos
 import SwiftUI
 
 struct DetailView: View {
@@ -16,40 +16,48 @@ struct DetailView: View {
                     .padding(8)
                 Color.teal
                     .padding(16)
-                VStack() {
-                    HStack() {
+                Color.white
+                    .padding(24)
+                
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
                         Spacer()
-                        Image("Messi")
+                        Image("Image")
                             .resizable()
-                            .padding()
                             .scaledToFill()
-                            .frame(width: 135, height: 180, alignment: .center)
+                            .frame(width: 150, height: 225, alignment: .center)
                             .clipped()
                     }
                     Spacer()
                 }
                 .padding(24)
                 .frame(width: 320)
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 0) {
                     
-                    Text("Lionel Messi")                    .font(.title)
+                    Text("Lionel Messi")
+                        .font(.title)
                         .bold()
-                        .padding(20)
+                        .frame(width: 122, height: 70, alignment: .leading)
+                        .padding(.leading, 24)
+                        .padding(.top, 18)
                     
                     
                     
                     VStack {
                         Text("Career Stats")
                             .bold()
+                            .font(.headline)
                         
                         HStack {
                             VStack(alignment: .leading) {
+                                Text("Height")
                                 Text("Position:")
                                 Text("Number:")
                                 Text("Games:")
                                 Text("Assists:")
                                 Text("Goals:")
                             }
+                            Spacer()
                             VStack(alignment: .trailing) {
                                 Text("FW")
                                 Text("30")
@@ -59,10 +67,16 @@ struct DetailView: View {
                             }
                         }
                     }
-                    .padding(.horizontal, 24)
-                    Text("Born in Rosario, Argentina 24 June 1987. Competitive debut in 2004 playing for Barcelona FC. Played in said team until 2021, when he joined Paris Saint-Germain. Team captain of the Argentina National Team.")
+                    .padding(.vertical, 8)
+                    .padding(.leading, 24)
+                    .padding(.trailing, 182)
+                    Rectangle()
+                        .fill(Color.teal)
+                        .frame(width: 288, height: 8)
+                        .padding(.horizontal, 16)
+                    Text("Born in Rosario, Argentina 24 June 1987. Competitive debut in 2004 playing for Barcelona FC. Joined PSG in 2021. Team captain of the Argentina National Team.")
                         .font(.body)
-                        .padding(.top, 10)
+                        .padding(.vertical, 15)
                         .padding(.horizontal, 24)
                     
                     HStack {
